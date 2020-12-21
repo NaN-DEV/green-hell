523d772328f9d4538ea7b3babd36cd5f7debbd5c
NaN <nan@MacBook-Air-NaN.local>
2020-12-21 15:26:22 +0100

    add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,29 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Green Hell Blog',
+      title: 'Green Hell Blog ( Index )',
+      filename: 'index.html', // output file
+      template: path.resolve(__dirname, './src/index.html'), // template file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Green Hell Blog  ( About )',
+      filename: 'about.html', // output file
+      template: path.resolve(__dirname, './src/about.html'), // template file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Green Hell Blog  ( contact )',
+      filename: 'contact.html', // output file
+      template: path.resolve(__dirname, './src/contact.html'), // template file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Green Hell Blog  ( newsList )',
+      filename: 'newsList.html', // output file
+      template: path.resolve(__dirname, './src/newsList.html'), // template file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Green Hell Blog  ( newsPost )',
+      filename: 'newsPost.html', // output file
+      template: path.resolve(__dirname, './src/newsPost.html'), // template file
     }),
   ],
   output: {
